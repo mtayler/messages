7fdba358ec38d96fe5ca9f1ae99762be597c9135
Tayler Mulligan <mulligantayler@gmail.com>
2016-07-19 18:59:33 -0400

    Fix gaps in graph when no messages
    + Collapes `messages` into `get-messages`

--- plot-messages.py
+++ plot-messages.py
@@ -1,27 +1,44 @@
-#!/usr/local/bin/python
-
+#!/usr/bin/env python
 import datetime
 import matplotlib.pyplot as plt
-from collections import Counter
 
-dt = datetime.datetime(2000, 12, 31, 16, 0)
+
+def dayrange(start_date, end_date):
+    dates = {}
+    day_delta = end_date - start_date
+    for n in xrange(day_delta.days + 1):
+        dates[start_date + datetime.timedelta(days=n)] = 0
+    return dates
+
+epoch = datetime.datetime(2000, 12, 31, 16, 0)
 
 with open('message_dates.dat') as f:
     lines = f.read().splitlines()
 
-todate = lambda sec: dt + datetime.timedelta(seconds=int(sec))
+todate = lambda sec: epoch + datetime.timedelta(seconds=int(sec))
+raw_dates = [todate(line).date() for line in lines]
 
-dates = [todate(line) for line in lines]
-isodates = [date.strftime("%Y-%m-%d") for date in dates]
+print "Min: {}".format(min(raw_dates))
+print "Max: {}".format(max(raw_dates))
 
-data = sorted(Counter(isodates).items())
+dates = dayrange(min(raw_dates), max(raw_dates))
 
-l = range(len(data))
+for raw_date in raw_dates:
+    dates[raw_date] += 1
+
+for date in dates:
+    date = date.strftime("%Y-%m-%d")
+
+date_list = sorted(dates)
+
+# Graph rendering stuff
+l = range(len(date_list))
 skip = int(max(len(l) / 20, 1))
 
-print("Max: {0}".format(max([(point[1], point[0]) for point in data])))
+print("Max: {0}".format(max([(dates[point], point) for point in dates])))
 
-plt.bar(l, [item[1] for item in data], align='center')
-plt.xticks(l[::skip], [item[0] for item in data[::skip]], rotation=70)
+plt.bar(l, [dates[day] for day in date_list], align='center')
+plt.xticks(l[::skip],
+           [key for key in date_list[::skip]], rotation=70)
 plt.tight_layout()
 plt.show()
